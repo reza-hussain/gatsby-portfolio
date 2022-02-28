f6ecc525caaebdcc1ad521766135a9b592c1a9e6
Reza Hussain <alirezaa08@gmail.com>
2022-02-28 22:34:32 +0530

    Commit-1

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <h1>Contact</h1>
+            <p>
+                The best way to reach me is via <a href="https://twitter.com/@andrew_j_mead" target="_blank">@andrew_j_mead</a> on Twitter!
+            </p>
+        </Layout>
+    )
+}
+
+export default ContactPage
