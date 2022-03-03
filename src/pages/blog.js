44aceea4c1aa106d203a0ddc679968c80fcf9ade
Reza Hussain <alirezaa08@gmail.com>
2022-03-03 17:46:37 +0530

    Commit-4

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
+
+
 
 const BlogPage = () => {
 
@@ -16,6 +18,9 @@ const BlogPage = () => {
                         }
                         html
                         excerpt
+                        fields{
+                            slug
+                        }
                     }
                 }
             }
@@ -32,7 +37,8 @@ const BlogPage = () => {
                     {data.allMarkdownRemark.edges.map((edge) => {
                         return(
                             <li>
-                                <h2>{edge.node.frontmatter.title}</h2>
+                                
+                                <Link to={`/blog/${edge.node.fields.slug}`}><h2>{edge.node.frontmatter.title}</h2></Link>
                                 <p>{edge.node.frontmatter.date}</p>
                             </li>
                         )
---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Blog = () => {
+  return (
+    <Layout>
+        This is a blog template
+    2</Layout>
+  )
+}
+
+export default Blog
